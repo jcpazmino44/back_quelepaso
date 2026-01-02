6c7f1be40656e7063dd061c51c2a609b8fba312f
jcpazmino <jcpazmino44@gmail.com>
2026-01-02 10:18:23 -0500

    quitar console de variable

--- src/config/env.js
+++ src/config/env.js
@@ -1,7 +1,6 @@
 const dotenv = require('dotenv');
 
 dotenv.config();
-console.log('METRICS_API_KEY:', process.env.METRICS_API_KEY);
 
 const getEnv = (key, fallback) => {
   const value = process.env[key];
