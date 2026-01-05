1e33610d2b0d8b8d83b3403b00f28b228885edb8
jcpazmino <jcpazmino44@gmail.com>
2026-01-05 16:35:15 -0500

    casteé platform al enum en el UPSERT para evitar el error

--- src/models/device.model.js
+++ src/models/device.model.js
@@ -3,7 +3,7 @@ const db = require('../config/db');
 const upsertByUuid = async ({ device_uuid, platform, app_version, city, zone }) => {
   const result = await db.query(
     `INSERT INTO devices (device_uuid, platform, app_version, city, zone, last_seen_at)
-     VALUES ($1, COALESCE($2, 'android'), $3, $4, $5, NOW())
+     VALUES ($1, COALESCE($2::device_platform_enum, 'android'::device_platform_enum), $3, $4, $5, NOW())
      ON CONFLICT (device_uuid)
      DO UPDATE SET
        platform = COALESCE(EXCLUDED.platform, devices.platform),
