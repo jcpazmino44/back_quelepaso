9becc4196eefd7559c62566dab4fc76cce17b87f
jcpazmino <jcpazmino44@gmail.com>
2026-01-13 08:33:44 -0500

    el post sigue presentando errores, se corrigieron

--- src/routes/diagnostic.routes.js
+++ src/routes/diagnostic.routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { create, updateStatus } = require('../controllers/diagnostic.controller');
+const multer = require('multer');
 
 const router = express.Router();
+const upload = multer();
 
-router.post('/', create);
+router.post('/', upload.none(), create);
 router.patch('/:id/status', updateStatus);
 
 module.exports = router;
