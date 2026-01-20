8ce7c55ee4610e2822a536db4aa8181c408530de
jcpazmino <jcpazmino44@gmail.com>
2026-01-20 14:44:22 -0500

    cambio de servidor a railway

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,20 @@
 const { Pool } = require('pg');
 const env = require('./env');
 
-const pool = new Pool({
+//conexión para render
+/* const pool = new Pool({
   connectionString: env.db.url,
   ssl: env.db.ssl
+}); */
+
+
+//conexión para Railway
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production'
+    ? { rejectUnauthorized: false }
+    : false,
 });
+
 
 module.exports = pool;
